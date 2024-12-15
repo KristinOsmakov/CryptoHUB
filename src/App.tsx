f95e1e877bbf5d1d7106d91a69a00cda5428c2bc
KristinOsmakov <oskristinov@gmail.com>
2024-12-16 00:24:31 +0300

    feat: delete Modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import { Provider } from 'react-redux'
 import './App.css'
 import { CryptoExchanges } from './shared/ui/CryptoExchanges'
 import { store } from './store/store'
-import { Modal } from './shared/ui/Modal'
 
 function App() {
 
   return (
     <Provider store={store}>
       <CryptoExchanges/>
-      <Modal/>
     </Provider>
   )
 }
